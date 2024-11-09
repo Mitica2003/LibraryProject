66e509223d9ad3d187d93d249b3af69dc2925020
Mitica <ardeleanmitica2016@gmail.com>
2024-11-09 18:24:16 +0200

    Added initial project save and delete functionalities with Uber Jar

---
+++ src/test/java/BookRepositoryMySQLTest.java
@@ -0,0 +1,42 @@
+import database.DatabaseConnectionFactory;
+import model.Book;
+import model.builder.BookBuilder;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import repository.BookRepository;
+import repository.BookRepositoryMock;
+import repository.BookRepositoryMySQL;
+
+import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BookRepositoryMySQLTest {
+    private static Connection connection = null;
+    private static BookRepository bookRepository;
+    @BeforeAll
+    public static void setup(){
+        connection = DatabaseConnectionFactory.getConnectionWrapper(false).getConnection();
+        bookRepository = new BookRepositoryMySQL(connection);
+    }
+
+    @Test
+    public void findAll(){
+        List<Book> books = bookRepository.findAll();
+        assertEquals(1, books.size());
+    }
+
+    @Test
+    public void findById(){
+        final Optional<Book> book = bookRepository.findById(1L);
+        assertTrue(!book.isEmpty());
+    }
+
+    @Test
+    public void save(){
+        assertTrue(bookRepository.save(new BookBuilder().setTitle("Ion").setAuthor("Liviu Rebreanu").setPublishedDate(LocalDate.of(1900, 10, 2)).build()));
+    }
+}
